83c07f86a313da2dcf488487e1b8715dae3bf6ae
Marco Zullich <marco.zullich@gmail.com>
2020-05-27 11:56:26 +0200

    Added resnet formulation

---
+++ resnet.py
@@ -0,0 +1,40 @@
+from core import *
+from torch_backend import *
+
+
+def conv_bn(c_in, c_out):
+    return {
+        'conv': nn.Conv2d(c_in, c_out, kernel_size=3, stride=1, padding=1, bias=False), 
+        'bn': BatchNorm(c_out), 
+        'relu': nn.ReLU(True)
+    }
+
+def residual(c):
+    return {
+        'in': Identity(),
+        'res1': conv_bn(c, c),
+        'res2': conv_bn(c, c),
+        'add': (Add(), ['in', 'res2/relu']),
+    }
+
+def net(channels=None, weight=0.125, pool=nn.MaxPool2d(2), extra_layers=(), res_layers=('layer1', 'layer3')):
+    channels = channels or {'prep': 64, 'layer1': 128, 'layer2': 256, 'layer3': 512}
+    n = {
+        'input': (None, []),
+        'prep': conv_bn(3, channels['prep']),
+        'layer1': dict(conv_bn(channels['prep'], channels['layer1']), pool=pool),
+        'layer2': dict(conv_bn(channels['layer1'], channels['layer2']), pool=pool),
+        'layer3': dict(conv_bn(channels['layer2'], channels['layer3']), pool=pool),
+        'pool': nn.MaxPool2d(4),
+        'flatten': Flatten(),
+        'linear': nn.Linear(channels['layer3'], 10, bias=False),
+        'logits': Mul(weight),
+    }
+    for layer in res_layers:
+        n[layer]['residual'] = residual(channels[layer])
+    for layer in extra_layers:
+        n[layer]['extra'] = conv_bn(channels[layer], channels[layer])       
+    return n
+
+def net_half(device = "cuda" if torch.cuda.is_available() else "cpu"):
+    return Network(net()).to(device).half()
